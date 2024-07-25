a9fe20a9fb15e95127a9f0af30c36b8d60386ccf
Josh Porter <josh@joshporter.xyz>
2024-07-25 13:29:48 -0500

    Add raylib boilerplate

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
-	fmt.Println("Sorting Visualization")
+	rl.InitWindow(800, 800, "Sorting Visualization")
+
+	rl.SetTargetFPS(60)
+
+	defer rl.CloseWindow()
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.Black)
+		rl.DrawText("Sorting Visualization", 0, 0, 20, rl.Blue)
+
+		rl.EndDrawing()
+	}
 }
